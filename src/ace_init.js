31d418f79e5a39e6ed99d9ffccf0ed5d6022d725
Etienne Noël <etiennenoel@google.com>
2024-03-29 15:45:55 -0700

    - Added dependencies.

---
+++ src/ace_init.js
@@ -0,0 +1,9 @@
+var editor = ace.edit("sql-editor", {
+  mode: "ace/mode/sql",
+  selectionStyle: "text",
+
+  fontFamily: "Fira Code",
+  fontSize: "10pt",
+
+});
+editor.setTheme("ace/theme/xcode");
